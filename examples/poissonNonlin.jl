854c74a1f8ffe8db54220ce340485348c2b3a7c7
Vedant Puri <vedantpuri@gmail.com>
2021-06-01 18:34:30 -0500

    todo: boundary condition

--- examples/poissonNonlin.jl
+++ examples/poissonNonlin.jl
@@ -2,8 +2,10 @@
 
 using Revise
 using SEM
-using LinearAlgebra,Plots,BenchmarkTools
+using LinearAlgebra,Plots,UnPack
 
+#----------------------------------#
+# Set up grid
 #----------------------------------#
 Ex = 8; nr1 = 8;
 Ey = 8; ns1 = 8;
@@ -19,32 +21,52 @@ ifperiodic = [false,false] # [xdir, ydir]
 m1 = Mesh(nr1,ns1,Ex,Ey,deform,ifperiodic)
 
 #----------------------------------#
-# solve with Newton Raphson approximation
+# case setup
+#----------------------------------#
+# solve with Newton Raphson approach
 #
-# ∇²u + G(u,x) = f(x)
+# ν∇²u + G(u,x) = f(x), x∈Ω
+#     u = ub,         , x∈∂Ω
+#
 #----------------------------------#
 x1 = m1.x
 y1 = m1.y
 
-G(v,msh::Mesh) = @. v^2
-Gdu(v,mesh::Mesh) = @. 2v
+function G(v,msh::Mesh) # G(u,x⃗)
+    @unpack x,y = msh
+    Gu = @. v^3
+    return Gu
+end
+function Gdu(v,msh::Mesh) # d/du G(u,x⃗)
+    @unpack x,y = msh
+    Gdu = @. 3v^2
+    return Gdu
+end
 
+# contrived example
 ν  = @. 1+0*x1
-
 kx = 2.0
 ky = 2.0
-ut = @. sin(kx*pi*x1)*sin(ky*pi*y1) # true solution
-f  = @. ut*((kx^2+ky^2)*pi^2)       # forcing/RHS
+ut = @. 0 + sin(kx*pi*x1)*sin(ky*pi*y1) # true solution
+f  = @. ut*((kx^2+ky^2)*pi^2)           # forcing/RHS
 f += G(ut,m1)
 
-ub  = @. 0+0*x1 # boundary data
-δub = @. 0+0*x1
+u  = @. 0+0*x1 # initial guess (must agree with boundary data)
+δu = @. 0+0*x1 # initialize
 
+# boundary condition
+# 'N': Neumann (homogeneous)
+# 'D': Dirichlet (inhomogeniety applied through boundary data)
 M1 = generateMask(['D','D','D','D'],m1) # [xmin,xmax,ymin,ymax]
 
+#----------------------------------#
+# solve homogeneous linearized equation for δu
+#----------------------------------#
 function residual(v,msh::Mesh)
     resi  = mass(f,msh)
     resi -= mass(G(v,msh),msh) .+ ν.*lapl(v,msh)
+    resi .= mask(resi,M1)
+    resi .= gatherScatter(resi,m1)
     return resi
 end
 
@@ -55,9 +77,7 @@ function opHlmz(v,k,msh::Mesh) # LHS op
     return Hu
 end
 
-u  = @. 0+0*x1 # initial guess
-δu = @. 0+0*x1
-for i=1:10
+for i=1:100
     local g = G(u,m1)
     local k = Gdu(u,m1)
 
@@ -65,26 +85,20 @@ for i=1:10
     opLHS(v) = opHlmz(v,k,m1)
 
     # RHS
-    b  = mass(f,m1)
-    b -= ν.*lapl(u,m1) .+ mass(g,m1)
-    b  = mask(b,M1)
-    b  = gatherScatter(b,m1)
+    b  = mass(f,m1)                  # forcing
+    b -= ν.*lapl(u,m1) .+ mass(g,m1) # from LHS
+    b .= mask(b,M1)
+    b .= gatherScatter(b,m1)
 
     # solve
     pcg!(δu,b,opLHS,mult=m1.mult,ifv=false)
     u .+= δu
 
-    r  = residual(u,m1) # residual calculation is not adding up...
-    r .= mask(r,M1)
-    r .= gatherScatter(r,m1)
-
-    bb = norm(b ,Inf); println("At iter $i, RHS norm is $bb")
-    uu = norm(δu,Inf); println("At iter $i, δu  norm is $uu")
-    rr = norm(r ,Inf); println("At iter $i, res norm is $rr")
-
-    if(rr < 1e-6) break end
+    r  = residual(u,m1);rr = norm(r,Inf);println("iter $i, res norm $rr")
+    if(rr < 1e-8) break end
 end
 #----------------------------------#
+er=ut-u; er=norm(er,Inf); println("infinity norm of error is $er")
 plt = meshplt(u,m1)
 display(plt)
 #----------------------------------#
