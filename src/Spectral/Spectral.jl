651794fb1c20a98f57e35bffcb0e3944b145d45a
Vedant Puri <vedantpuri@gmail.com>
2022-01-18 03:54:09 -0500

    define eltype for all spaces

--- src/Spectral/Spectral.jl
+++ src/Spectral/Spectral.jl
@@ -9,11 +9,20 @@ import Base: similar, copy, copy!                           # allocation
 import Base: length, size, getindex, setindex!, IndexStyle  # indexing
 import Base.Broadcast: BroadcastStyle                       # broadcast
 import Base: +, -, *, /, \, adjoint                         # math
-import LinearAlgebra: mul!, ldiv!, dot, norm#, axpy!, axpby!, diagonal, Diagonal
+import LinearAlgebra: mul!, ldiv!, dot, norm, Adjoint#, Diagonal
 
 abstract type AbstractSpectralField{T,N} <: AbstractVector{T} end
 abstract type AbstractSpectralOperator{T,N} end
 abstract type AbstractSpectralSpace{T,N} end
+
+eltype(u::AbstractSpectralField{T,N}) where{T,N} = T
+eltype(u::AbstractSpectralOperator{T,N}) where{T,N} = T
+eltype(u::AbstractSpectralSpace{T,N}) where{T,N} = T
+
+function (A::AbstractSpectralOperator{T,N})(u)
+    v = similar(u)
+    mul!(v,A,u)
+end
 
 include("TensorProdField.jl")
 include("TensorProdOperator.jl")
