7481aee13a47d75aef9b0ab80077803cd47ea9a4
Vedant Puri <vedantpuri@gmail.com>
2022-01-27 12:04:41 -0500

    more org

--- src/Spectral/Spectral.jl
+++ src/Spectral/Spectral.jl
@@ -24,39 +24,7 @@ Base.eltype(u::Union{AbstractSpectralField{T,N},
                      AbstractSpectralSpace{T,N}}
            ) where{T,N} = T
 
-""" utilize Base.ReshaedArray """
-_reshape(a,dims::NTuple{N,Int}) where{N} = reshape(a,dims) # fallback
-_reshape(a::Array, dims::NTuple{N,Int}) where{N} = Base.ReshapedArray(a, dims, ())
-#
-_vec(a::AbstractVector) = a
-_vec(a::AbstractArray) = _reshape(a,(length(a),))
-
-""" check if operator(s) is square """
-issquare(::UniformScaling) = true
-issquare(A::AbstractMatrix) = size(A,1) === size(A,2)
-issquare(A...) = @. (&)(issquare(A)...)
-
-""" Identity Matrix with the notion of size """
-struct Identity{Ti,Tn} #<: AbstractMatrix{Bool}
-  I::Ti
-  n::Tn
-  #
-  function Identity(n)
-    id = I
-    new{typeof(id),typeof(n)}(id,n)
-  end
-end
-Base.size(Id::Identity) = (Id.n, Id.n)
-Base.eltype(::Identity) = Bool
-Base.adjoint(Id::Identity) = Id
-#
-(::Identity)(u) = u
-(*)(::Identity, u) = copy(u) # unnecessary if AbstractMatrix
-LinearAlgebra.mul!(v, id::Identity, u) = mul!(v, id.I, u)
-LinearAlgebra.ldiv!(v, id::Identity, u) = ldiv!(v, id.I, u)
-LinearAlgebra.ldiv!(id::Identity, u) = ldiv!(id.I, u)
-
-# includes
+include("misc_utils.jl")
 include("TensorProdField.jl")
 include("OperatorBasics.jl")
 include("TensorProdOperator.jl")
