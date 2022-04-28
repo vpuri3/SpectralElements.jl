0d47e0bf326dee12e97e9f16de20188f18fbe829
Vedant Puri <vedantpuri@gmail.com>
2022-04-28 08:38:34 -0400

    cleanup

--- src/Spectral/GalerkinOperators.jl
+++ src/Spectral/GalerkinOperators.jl
@@ -9,6 +9,17 @@ struct GatherScatter{D} <: AbstractGatherScatterOperator{Bool,D}
     implementation
 end
 
+"""
+Q*Q'*u where Q: local -> global operator
+"""
+function DSS(u,l2g,g2l)
+    
+    Qu   = NNlib.scatter(+,u,l2g)
+    QQtu = NNlib.gather(Qu,g2l)
+
+    return v
+end
+
 function Qmatrix(n::Integer, periodic::Bool)
     Q = sparse(I,n, n-1)
 
